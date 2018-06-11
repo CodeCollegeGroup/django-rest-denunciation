8fa17b643c2a03ae960923877aa19116fb418fd1
Vinicius Ferreira <vfbdelima@gmail.com>
2018-06-11 08:39:39 -0300

    Add domain administrator
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- django_rest_denunciation/domain/models.py
+++ django_rest_denunciation/domain/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class DomainAdministrator(User):
+
+    def recover_domain_key(self, domain_key=None):
+        pass
+
+    def recover_password(self):
+        pass
+
+    def retrieve_denunciation_by_priority(self, priority_type=None):
+        pass
 
 
 class Domain(models.Model):
+
+    administrator = models.ForeignKey(
+        DomainAdministrator,
+        on_delete=models.CASCADE,
+        null=True
+    )
 
     uri = models.URLField(max_length=150)
 
