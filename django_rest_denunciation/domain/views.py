8a99ef6b1dd257fa3ec4e0fc7cb6fab6797f6cdf
Vinicius Ferreira <vfbdelima@gmail.com>
2018-06-12 22:48:54 -0300

    Add recover domain key endpoint which send the key to the domain admin
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Co-authored-by: Ícaro Pires <icaropsa@gmail.com>

--- django_rest_denunciation/domain/views.py
+++ django_rest_denunciation/domain/views.py
@@ -1,7 +1,6 @@
 from smtplib import SMTPException
-from json import dumps
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Domain, DomainAdministrator
@@ -33,13 +32,40 @@ class DomainAdministratorViewSet(viewsets.ModelViewSet):
             user.recover_password()
         except ObjectDoesNotExist:
             response = Response(
-                dumps({'detail': 'user not found'}),
-                status=404
+                {'detail': 'user not found'},
+                status.HTTP_404_NOT_FOUND
             )
         except SMTPException:
             response = Response(
-                dumps({'detail': 'error while sending email'}),
-                status=500
+                {'detail': 'error while sending email'},
+                status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        return response
+
+    @action(methods=['get'], detail=False)
+    def recover_domain_key(self, request):
+        response = Response()
+        data = request.data
+        try:
+            domain = Domain.objects.get(
+                application_name=data.get('application_name')
+            )
+            admin = domain.administrator
+            message = """Olá,\nA chave do seu domínio '{}' é\n
+                         Chave: {}""".format(domain.uri, domain.key)
+            admin.send_email(
+                'Equipe Django Rest Denunciation',
+                message
+            )
+        except ObjectDoesNotExist:
+            response = Response(
+                {'detail': 'domain not found'},
+                status.HTTP_404_NOT_FOUND
+            )
+        except SMTPException:
+            response = Response(
+                {'detail': 'error while sending email'},
+                status.HTTP_500_INTERNAL_SERVER_ERROR
             )
         return response
 
@@ -52,5 +78,5 @@ class DomainAdministratorViewSet(viewsets.ModelViewSet):
                 password=data.get('password')
             )
         except ValidationError:
-            response = Response(status=500)
+            response = Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return response
