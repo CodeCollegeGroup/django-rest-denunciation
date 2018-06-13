a5b51765d521db01dbfc4977b76ebc8d39796a1b
Vinicius Ferreira <vfbdelima@gmail.com>
2018-06-12 22:44:34 -0300

    Add application name to domain and separate send email function
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Co-authored-by: Ícaro Pires <icaropsa@gmail.com>

--- django_rest_denunciation/domain/models.py
+++ django_rest_denunciation/domain/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage
 
 
 class DomainAdministrator(User):
-
-    def recover_domain_key(self, domain_key=None):
-        pass
 
     def recover_password(self):
         """Reset password sending in e-mail"""
@@ -14,18 +11,17 @@ class DomainAdministrator(User):
         random_password = DomainAdministrator.objects.make_random_password()
         self.set_password(random_password)
         self.save()
-
-        """
-        message = ""Olá,\nSua senha foi resetada, acesse
+        message = """Olá,\nSua senha foi resetada, acesse
                      a plataforma e troque a senha\n
-                     Sua nova senha é:\n {}"".format(random_password)
-        email = EmailMessage('Password reset',
-                             message, to=[self.email])
-        email.send()
-        """
+                     Sua nova senha é:\n {}""".format(random_password)
+        self.send_email('Password Reset', message)
 
     def retrieve_denunciation_by_priority(self, priority_type=None):
         pass
+
+    def send_email(self, title, message):
+        email = EmailMessage(title, message, to=[self.email])
+        email.send()
 
 
 class Domain(models.Model):
@@ -35,6 +31,8 @@ class Domain(models.Model):
         on_delete=models.CASCADE,
         null=True
     )
+
+    application_name = models.CharField(unique=True, max_length=50)
 
     uri = models.URLField(max_length=150)
 
