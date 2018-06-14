34673357689bb463fb5b03aa12b6e537ded3c311
mendesiasmin <mendesiasmin96@gmail.com>
2018-06-14 09:04:32 -0300

    Adiciona política de throttle padrao

--- django_rest_denunciation/django_rest_denunciation/settings.py
+++ django_rest_denunciation/django_rest_denunciation/settings.py
@@ -111,6 +111,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
