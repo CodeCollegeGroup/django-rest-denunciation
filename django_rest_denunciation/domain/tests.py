cab4ae7cde01ef0d507c3e0c0902b84f7deae05d
Ícaro Pires <icaropsa@gmail.com>
2018-06-14 18:57:23 -0300

    Add test for creating domain objects through requisition on happy flow

--- django_rest_denunciation/domain/tests.py
+++ django_rest_denunciation/domain/tests.py
@@ -38,3 +38,27 @@ class DomainModelsTests(test.TestCase):
         )
 
         self.assertTrue(len(domain.key) == KEY_LENGTH)
+
+
+class DomainViewsTests(test.TestCase):
+
+    def setUp(self):
+        self.admin = DomainAdministratorFactory.create()
+        self.client_test = test.Client()
+
+    def test_create_domain_ok(self):
+        username = self.admin.username
+        application_name = 'mywebsite'
+        uri = 'http://www.mywebsite.com.br'
+
+        response = self.client_test.post(
+            '/api/domains/domains/',
+            {'username': username,
+             'application_name': application_name,
+             'uri': uri}
+        )
+        last_saved = Domain.objects.last()
+
+        self.assertEqual(application_name, last_saved.application_name)
+        self.assertEqual(self.admin, last_saved.administrator)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
