990dd6f5761afeafb34c31e494c80229ecc8c9cd
Ícaro Pires <icaropsa@gmail.com>
2018-06-15 16:59:11 -0300

    Add routes for denunciation app
    Co-authored-by: matias0422 <leguenta@hotmail.com>

---
+++ django_rest_denunciation/denunciation/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from rest_framework import routers
+from .views import (
+    DenunciationViewSet,
+    DenunciableViewSet,
+    DenunciationCategoryViewSet,
+    DenunciationCompleteList
+)
+
+ROUTER = routers.DefaultRouter()
+ROUTER.register(r'denunciation', DenunciationViewSet)
+ROUTER.register(r'denunciable', DenunciableViewSet)
+ROUTER.register(r'denunciation-category', DenunciationCategoryViewSet)
+
+urlpatterns = ROUTER.urls  # pylint: disable=invalid-name
+
+urlpatterns += [
+    url(r'denunciation-complete', DenunciationCompleteList.as_view()),
+    # url(r'denunciation/(?P<pk>[0-9]+)/$', DenunciationDetails.as_view()),
+]
