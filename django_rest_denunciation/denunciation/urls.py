42e6e21177a2a089f3b9b7d525abf5f210244d20
Daniel Marques <danielmarques7@hotmail.com>
2018-06-17 16:59:46 -0300

    Add denouncer route
    Co-authored-by: wallacybraz <wallacy.braz@live.com>
    Co-authored-by: matias0422 <leguenta@hotmail.com>

--- django_rest_denunciation/denunciation/urls.py
+++ django_rest_denunciation/denunciation/urls.py
@@ -1,9 +1,33 @@
 from django.conf.urls import url
 from denunciation.views import DenunciationList, DenunciationDetails
+from rest_framework import routers
+from .views import (
+    DenunciableViewSet,
+    DenunciationCategoryViewSet,
+    DenouncerViewSet,
+    change_denunciation_state
+)
 
-urlpatterns = [
-    url(r'denunciation/$', DenunciationList.as_view(),
-        name='denunciation-list'),
-    url(r'denunciation/(?P<pk>[0-9]+)/$', DenunciationDetails.as_view(),
-        name='denunciation-detail'),
+ROUTER = routers.DefaultRouter()
+ROUTER.register(r'denunciable', DenunciableViewSet)
+ROUTER.register(r'denunciation-category', DenunciationCategoryViewSet)
+ROUTER.register(r'denouncer', DenouncerViewSet)
+
+urlpatterns = ROUTER.urls
+
+urlpatterns += [
+    url(
+        r'denunciation/$',
+        DenunciationList.as_view(),
+        name='denunciation-list'
+    ),
+    url(
+        r'denunciation/(?P<pk>[0-9]+)/$',
+        DenunciationDetails.as_view(),
+        name='denunciation-detail'
+    ),
+    url(
+        r'denunciation/(?P<pk>[0-9]+)/(?P<name>[a-z]+)/$',
+        change_denunciation_state
+    )
 ]
