a2eea161babf4311c134b9f3e72505e6c4018d2b
mendesiasmin <mendesiasmin96@gmail.com>
2018-06-17 18:25:44 -0300

    Implements throttle validation to denouncer

--- django_rest_denunciation/django_rest_denunciation/throttle.py
+++ django_rest_denunciation/django_rest_denunciation/throttle.py
@@ -1,6 +1,12 @@
 from rest_framework.throttling import (
     UserRateThrottle
 )
+from denunciation.models import Denunciation
+from datetime import (
+    datetime,
+    date,
+    time
+)
 import json
 
 
@@ -16,9 +22,26 @@ class DenouncerRateThrottle(UserRateThrottle):
 
         data = json.loads(request.body.decode('utf-8'))
 
-        if 'denouncer' in data:
-            denouncer = data['denouncer']
-            #TODO check if has many denouncements of denouncer in day
-            return super(DenouncerRateThrottle, self).allow_request(request, view)
+        if 'denouncer' in data and 'email' in data['denouncer']:
+            denouncer = data['denouncer']['email']
+            domain = data['denunciation']['domain']
+
+            if domain[-1] == '/':
+                domain = domain[:-1]
+            domain = domain.rsplit('/')[-1]
+
+            start_day = datetime.combine(date.today(), time.min)
+            end_day = datetime.combine(date.today(), time.max)
+
+            denunciations = Denunciation.objects.filter(
+                domain=domain,
+                denouncer__email=denouncer,
+                denunciable__denunciable_datetime__range=(start_day, end_day)
+            )
+
+            if denunciations.count() < self.num_requests:
+                return True
+            else:
+                return False
         else:
             return False
